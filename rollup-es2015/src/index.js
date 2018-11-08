103b3b1f10c97ca36ab4bcf101047429328a0d2d
wyrover <wyrover@gmail.com>
2018-11-08 10:03:46 +0800

    + jquery

--- rollup-es2015/src/index.js
+++ rollup-es2015/src/index.js
@@ -1,4 +1,7 @@
 import demo from './lib/demo'
+import jquery from 'jquery'
+
+window.$ = jquery
 
 const arr1 = [1, 2, 3]
 const arr2 = [4, 5, 6]
@@ -6,3 +9,7 @@ console.log([...arr1, ...arr2])
 
 console.log('test1')
 demo.init()
+
+console.log($)
+
+$('body').append('test 456789')
