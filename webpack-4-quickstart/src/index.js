7df3eaded1f3babc4abb3a91279dd137cd19718a
wyrover <wyrover@gmail.com>
2018-11-08 15:02:08 +0800

    + webpack-4-quickstart

---
+++ webpack-4-quickstart/src/index.js
@@ -0,0 +1,7 @@
+import style from './main.css'
+
+console.log('hello world!')
+
+const arr = [1, 2, 3]
+const iAmJavascriptES6 = () => console.log(...arr)
+window.iAmJavascriptES6 = iAmJavascriptES6
